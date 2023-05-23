371b9de1fb1bcfd3c2f63207586cf1deecc5bff6
Alexandru Iova <alex.yova97@gmail.com>
2023-05-23 19:52:34 +0200

    initial commit

---
+++ src/main/java/com/example/sanitastest/SanitastestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanitastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitastestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanitastestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sanitastest/SanitastestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanitastest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanitastestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
