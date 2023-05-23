177aee00c2488043687eb8f24fde04953779946e
Alexandru Iova <alex.yova97@gmail.com>
2023-05-23 20:36:46 +0200

    resfactor calculator service test

--- src/test/java/com/example/sanitastest/service/CalculatorServiceTest.java
+++ src/test/java/com/example/sanitastest/service/CalculatorServiceTest.java
@@ -8,17 +8,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class CalculatorServiceTest {
+    CalculatorService calculatorService = new CalculatorService();
 
     @Test
     void shouldSumUpTwoValues() {
-        CalculatorService calculatorService = new CalculatorService();
         BigDecimal result = calculatorService.sum(new BigDecimal(1), new BigDecimal(2));
 
         assertThat(result).isEqualTo(new BigDecimal(3));
     }
+
     @Test
     void shouldSubstractTheGivenValues() {
-        CalculatorService calculatorService = new CalculatorService();
         BigDecimal result = calculatorService.substract(new BigDecimal(20), new BigDecimal(10));
 
         assertThat(result).isEqualTo(new BigDecimal(10));
