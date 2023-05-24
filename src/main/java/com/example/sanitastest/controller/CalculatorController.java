919cef2e31377f6cfe98f9b764de0bf80b2d6832
Alexandru Iova <alex.yova97@gmail.com>
2023-05-24 11:30:38 +0200

    Add tracer

--- src/main/java/com/example/sanitastest/controller/CalculatorController.java
+++ src/main/java/com/example/sanitastest/controller/CalculatorController.java
@@ -1,9 +1,12 @@
 package com.example.sanitastest.controller;
 
 import com.example.sanitastest.service.CalculatorService;
+import io.corp.calculator.TracerAPI;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
@@ -13,12 +16,19 @@ import java.math.BigDecimal;
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
+    private TracerAPI tracerAPI;
+
     @PostMapping("/sum")
-    public BigDecimal sum(@RequestParam("num1") BigDecimal num1,@RequestParam("num2")BigDecimal num2){
-        return calculatorService.sum(num1, num2);
+    public BigDecimal sum(@RequestParam("num1") BigDecimal num1, @RequestParam("num2") BigDecimal num2) {
+        BigDecimal result = calculatorService.sum(num1, num2);
+        tracerAPI.trace(result);
+        return result;
     }
+
     @PostMapping("/substract")
-    public BigDecimal substract(@RequestParam("num1") BigDecimal num1,@RequestParam("num2")BigDecimal num2){
-        return calculatorService.substract(num1, num2);
+    public BigDecimal substract(@RequestParam("num1") BigDecimal num1, @RequestParam("num2") BigDecimal num2) {
+        BigDecimal result = calculatorService.substract(num1, num2);
+        tracerAPI.trace(result);
+        return result;
     }
 }
